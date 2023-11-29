f70a38ff78249be5265ffe4a026d68ad9f97ee93
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2023-11-29 14:31:48 +0100

    Update packer.lua

--- .config/nvim/lua/gateixeira/packer.lua
+++ .config/nvim/lua/gateixeira/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   
   use { 'ThePrimeagen/vim-be-good' }
-
+  use { 'williamboman/mason.nvim' }
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	  -- or                            , branch = '0.1.x',
